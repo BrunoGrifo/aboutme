602903f92c2adae61d3a6c5e1d6862aa3255d94d
Bruno Grifo <leitaogrifo@gmail.com>
2021-08-03 16:15:43 +0100

    DEBUG os env

--- aboutme/settings/prod.py
+++ aboutme/settings/prod.py
@@ -2,7 +2,7 @@ from aboutme.settings.base import *
 import json
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
