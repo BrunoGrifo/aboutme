a32e4739bd0b953ab9671b033d2644e298c68db6
Bruno Grifo <leitaogrifo@gmail.com>
2021-09-20 18:51:12 +0100

    logging in production

--- aboutme/settings/prod.py
+++ aboutme/settings/prod.py
@@ -40,7 +40,7 @@ AWS_S3_FILE_OVERWRITE = False
 DEFAULT_FILE_STORAGE = 'core.storages.MediaStore'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'dist')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
